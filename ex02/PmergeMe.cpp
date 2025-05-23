34daf455685b45f29ae149066d0c07f15cd9372d
Mehmet Kaan Akcan <mehakcan@k2m18s07.42kocaeli.com.tr>
2025-05-23 15:11:39 +0300

    230520251511Update

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,20 +1,15 @@
 #include "PmergeMe.hpp"
-#include <iostream>
 #include <sstream>
 #include <cstdlib>
-#include <ctime>
 
 PmergeMe::PmergeMe() {}
-
 PmergeMe::~PmergeMe() {}
 
 bool PmergeMe::isNumber(const std::string &str)
 {
     for (std::size_t i = 0; i < str.length(); ++i)
-    {
         if (!std::isdigit(str[i]))
             return false;
-    }
     return true;
 }
 
@@ -31,109 +26,6 @@ void PmergeMe::parseInput(int ac, char **av)
         _vec.push_back(n);
         _deq.push_back(n);
     }
-}
-
-void PmergeMe::printContainer(const std::string &label, const std::vector<int> &c)
-{
-    std::cout << label;
-    for (std::size_t i = 0; i < c.size(); ++i)
-    {
-        std::cout << c[i];
-        if (i != c.size() - 1)
-            std::cout << " ";
-    }
-    std::cout << std::endl;
-}
-
-void PmergeMe::printContainer(const std::string &label, const std::deque<int> &c)
-{
-    std::cout << label;
-    for (std::size_t i = 0; i < c.size(); ++i)
-    {
-        std::cout << c[i];
-        if (i != c.size() - 1)
-            std::cout << " ";
-    }
-    std::cout << std::endl;
-}
-
-template <typename T>
-int binarySearchPosition(const T &container, int value) {
-    int left = 0;
-    int right = static_cast<int>(container.size());
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-        if (container[mid] < value)
-            left = mid + 1;
-        else
-            right = mid;
-    }
-    return left;
-}
-
-template <typename T>
-void mergeContainer(T &container, int left, int mid, int right)
-{
-    T leftVec(container.begin() + left, container.begin() + mid + 1);
-    T rightVec(container.begin() + mid + 1, container.begin() + right + 1);
-
-    std::size_t i = 0, j = 0;
-    int k = left;
-
-    while (i < leftVec.size() && j < rightVec.size())
-    {
-        if (leftVec[i] <= rightVec[j])
-            container[k++] = leftVec[i++];
-        else
-            container[k++] = rightVec[j++];
-    }
-    while (i < leftVec.size())
-        container[k++] = leftVec[i++];
-    while (j < rightVec.size())
-        container[k++] = rightVec[j++];
-}
-
-template <typename T>
-void mergeSort(T &container, int left, int right)
-{
-    if (left >= right)
-        return;
-    int mid = left + (right - left) / 2;
-    mergeSort(container, left, mid);
-    mergeSort(container, mid + 1, right);
-    mergeContainer(container, left, mid, right);
-}
-
-template <typename T>
-void    genericFordJohnsonSort(T& container)
-{
-    if (container.size() <= 1)
-        return;
-    T mainChain;
-    T pending;
-
-    for (std::size_t i = 0; i + 1 < container.size(); i += 2) {
-        int a = container[i];
-        int b = container[i + 1];
-        if (a < b) {
-            mainChain.push_back(b);
-            pending.push_back(a);
-        } else {
-            mainChain.push_back(a);
-            pending.push_back(b);
-        }
-    }
-
-    if (container.size() % 2 != 0)
-        pending.push_back(container[container.size() - 1]);
-
-    mergeSort(mainChain, 0, mainChain.size() - 1);
-
-    for (std::size_t i = 0; i < pending.size(); ++i) {
-        int pos = binarySearchPosition(mainChain, pending[i]);
-        mainChain.insert(mainChain.begin() + pos, pending[i]);
-    }
-    container = mainChain;
 }
 
 void PmergeMe::sortAndMeasure()
@@ -158,5 +50,3 @@ void PmergeMe::sortAndMeasure()
     std::cout << "Time to process a range of " << _deq.size()
               << " elements with std::deque  : " << timeDeq << " us" << std::endl;
 }
-
-
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -4,24 +4,94 @@
 #include <vector>
 #include <deque>
 #include <string>
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
 
 class PmergeMe {
-    private:
-        std::vector<int> _vec;
-        std::deque<int> _deq;
-    public:
-        PmergeMe();
-        ~PmergeMe();
+private:
+    std::vector<int> _vec;
+    std::deque<int> _deq;
 
-        void parseInput(int ac, char** av);
-        void sortAndMeasure();
+public:
+    PmergeMe();
+    ~PmergeMe();
 
-    private:
-        bool isNumber(const std::string& str);
-        void printContainer(const std::string& label, const std::vector<int>& c);
-        void printContainer(const std::string& label, const std::deque<int>& c);
+    void parseInput(int ac, char** av);
+    void sortAndMeasure();
 
-        int binarySearchPosition(const std::vector<int> &vec, int value);
-    };
+    bool isNumber(const std::string& str);
+
+    template <typename Container>
+    void printContainer(const std::string& label, const Container &c) {
+        std::cout << label;
+        for (typename Container::const_iterator it = c.begin(); it != c.end(); ++it) {
+            std::cout << *it;
+            if (it + 1 != c.end())
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+template <typename T>
+int binarySearchPosition(const T &container, int value) {
+    int left = 0;
+    int right = static_cast<int>(container.size());
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (container[mid] < value)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return left;
+}
+
+template <typename T>
+void insertionSort(T &container) {
+    for (std::size_t i = 1; i < container.size(); ++i) {
+        int key = container[i];
+        int j = static_cast<int>(i) - 1;
+        while (j >= 0 && container[j] > key) {
+            container[j + 1] = container[j];
+            --j;
+        }
+        container[j + 1] = key;
+    }
+}
+
+template <typename T>
+void genericFordJohnsonSort(T &container) {
+    if (container.size() <= 1)
+        return;
+
+    T mainChain;
+    T pending;
+
+    for (std::size_t i = 0; i + 1 < container.size(); i += 2) {
+        int a = container[i];
+        int b = container[i + 1];
+        if (a < b) {
+            mainChain.push_back(b);
+            pending.push_back(a);
+        } else {
+            mainChain.push_back(a);
+            pending.push_back(b);
+        }
+    }
+
+    if (container.size() % 2 != 0)
+        pending.push_back(container.back());
+
+    insertionSort(mainChain);
+
+    for (std::size_t i = 0; i < pending.size(); ++i) {
+        int pos = binarySearchPosition(mainChain, pending[i]);
+        mainChain.insert(mainChain.begin() + pos, pending[i]);
+    }
+
+    container = mainChain;
+}
 
 #endif
